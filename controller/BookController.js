5483f569c7eace325a80aa2c1035cc3b8f39b2e5
Kang Ye Ram <kyj5730@gmail.com>
2025-05-27 17:14:10 +0900

    카테고리별, 신간, 도서 목록 조회

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,31 +1,32 @@
 const conn = require("../mariadb"); // db 모듈
 const { StatusCodes } = require("http-status-codes"); // status 코드
 
+// {카테고리 별, 신간 여부} 전체 도서 목록 조회
 const allBooks = (req, res) => {
-  let { category_id } = req.query;
+  let { category_id, news } = req.query;
 
-  if (category_id) {
-    let sql = "SELECT * FROM books WHERE category_id=?";
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      if (results.length) return res.status(StatusCodes.OK).json(results);
-      else return res.status(StatusCodes.NOT_FOUND).end();
-    });
-  } else {
-    // (전체) 요약된 도서 리스트
-    let sql = "SELECT * FROM books";
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      return res.status(StatusCodes.OK).json(results);
-    });
+  let sql = "SELECT * FROM books ";
+  let values = [];
+  if (category_id && news) {
+    sql +=
+      "WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    values = [category_id, news];
+  } else if (category_id) {
+    sql += "WHERE category_id=?";
+    values = category_id;
+  } else if (news) {
+    sql += "WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    values = news;
   }
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.length) return res.status(StatusCodes.OK).json(results);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
 };
 
 const bookDetail = (req, res) => {
